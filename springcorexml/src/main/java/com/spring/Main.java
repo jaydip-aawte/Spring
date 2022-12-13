f6aa48194e90f80a347378f741c6b68639de1e5a
jaydip aawte <connect.jaydip@gmail.com>
2022-12-13 09:58:34 +0530

    Xml MetaData Configuration

--- springcorexml/src/main/java/com/spring/Main.java
+++ springcorexml/src/main/java/com/spring/Main.java
@@ -1,7 +1,13 @@
 package com.spring;
-
+import com.spring.beans.User;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      var context=new ClassPathXmlApplicationContext("UserConfig.xml");
+      User obj=context.getBean(User.class);
+      //this time we are providing data through xml config file without any annotations
+        //this is old way to provide meta data to spring ioc container to handel objects and instatiate classes
+        System.out.println(obj.getName());
+        System.out.println(obj.getAddress());
     }
 }
--- springcorexml/src/main/java/com/spring/beans/User.java
+++ springcorexml/src/main/java/com/spring/beans/User.java
@@ -1,4 +1,25 @@
 package com.spring.beans;
 
 public class User {
+    private String name;
+    private String address;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
